c0e070d84ac4912b9b3a77c43908446a22394d49
Jeremy Felix D <me@jeremyfelix.com>
2016-03-13 15:15:14 +0000

    Connected /api/posts to layout UI Moved /api/posts routes into a controller Used a router to attach /api/posts routes to server.js

--- server.js
+++ server.js
@@ -6,30 +6,11 @@ var Post = require('./models/post.js');
 var app = express();
 app.use(bodyParser.json());
 
-app.get('/posts', function(req, res){
-    res.sendFile('/projects/MEAN-SocialNetwork/posts.html');
-});
+//Attach the api/posts controller
+app.use('/api/posts', require('./controllers/api/posts'));
 
-app.get('/api/posts', function(req, res){
-   Post.find(function(err, posts){
-       if(err){ return next(err); }
-       res.json(posts);
-   });
-});
-
-app.post('/api/posts', function(req, res, next){
-    console.log('Post Recieved');
-    console.log(req.body.username);
-    console.log(req.body.body);
-    
-    var post = new Post({
-        username: req.body.username,
-        body: req.body.body
-    });
-    post.save(function(err, post){
-        if(err){ return next(err); }
-        res.json(201, post);
-    });
+app.get('/', function(req, res){
+    res.sendFile('/projects/MEAN-SocialNetwork/layouts/posts.html');
 });
 
 app.listen(3000, function() {
